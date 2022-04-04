ebed81ca98d9d022d234bf5fda86b16280ff398e
Matheus <matheuscmanoel@gmail.com>
2022-04-04 01:33:37 -0300

    feat: add construct code and initial albums.

--- contracts/AlbumDuel.sol
+++ contracts/AlbumDuel.sol
@@ -1,11 +1,29 @@
-// SPDX-License-Identifier: UNLICENSED
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract AlbumDuel {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT.");
+    struct Album {
+        uint256 albumIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+    Album[] albums;
+
+    constructor(Album[] memory charAtts) {
+        for (uint256 i = 0; i < charAtts.length; i += 1) {
+            albums.push(charAtts[i]);
+            Album memory c = albums[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,26 @@
 const hre = require('hardhat');
 
-const main = async() => {
-    const gameContractFactory = await hre.ethers.getContractFactory('AlbumDuel');
-    const gameContract = await gameContractFactory.deploy();
-    await gameContract.deployed();
-    console.log(`contract deployed to: ${gameContract.address}`);
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory('AlbumDuel');
+  const gameContract = await gameContractFactory.deploy(
+    [
+        [0, "The Dark Side of The Moon", "https://ipfs.io/ipfs/QmV5HAwkiXFXWo4wiqwCJ4LjoNb74dGrWKKTzCmCM9qmu9?filename=dark.jpg", 100, 100, 100],
+        [1, "Aang", "https://i.imgur.com/xVu4vFL.png", 200, 200, 50],
+        [2, "Pikachu", "https://i.imgur.com/WMB6g9u.png", 300, 300, 25]
+    ]
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
 };
 
-const runMain = async() => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 runMain();
